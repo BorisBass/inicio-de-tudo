c73a22838582235e3054cd2e220dda68d9c455f8
BorisBass <liojes@gmail.com>
2023-06-02 19:47:02 +0000

    primeira página

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home() {
+    return <div><h1>Veja só Rafa, o que fiz seguindo este <a href="https://youtu.be/EW7m2WIvFgQ" target="_blank">link</a>!</h1>
+            <p>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/EW7m2WIvFgQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>            </p>
+            </div>
+}
+
+export default Home
